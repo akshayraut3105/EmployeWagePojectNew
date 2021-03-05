816515f63692b6421e65f18e98a273f5acba72ae
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-05 19:28:24 +0530

    [AKSHAY] added wage for NOKIA AND SAMSUNG

--- EmployeeWageNew/Program.cs
+++ EmployeeWageNew/Program.cs
@@ -1,27 +1,24 @@
 using System;
 
-namespace EmployeeWageNew
+namespace EmployeeWages
 {
-
-
-    class Progran
+    class Program
     {
-        //Constants
         public const int IS_PART_TIME = 1;
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 20;
         public const int MAX_HRS_IN_MONTH = 100;
-        public static void Main()
+        static void computeEmpWage(String company, int empRatePerHrs, int numOfWorkingDays, int maxHoursPerMonth)
         {
-            //variables
             int empHrs = 0;
-            int totalWorkingDays = 0;
             int totalEmpHrs = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+            int totalWorkingDays = 0;
+            while (empHrs <= MAX_HRS_IN_MONTH && totalWorkingDays <= NUM_OF_WORKING_DAYS)
             {
                 totalWorkingDays++;
                 Random random = new Random();
+
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
                 {
@@ -35,13 +32,19 @@ namespace EmployeeWageNew
                         empHrs = 0;
                         break;
                 }
-
                 totalEmpHrs += empHrs;
-                Console.WriteLine("Day#" + totalWorkingDays + "EmpHrs:" + empHrs);
+                Console.WriteLine("Days:" + totalWorkingDays + "emp Hrs:" + empHrs);
             }
-            Console.WriteLine(totalEmpHrs + "   " + totalWorkingDays);
+
             int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+            Console.WriteLine("Total Emp Wage for Company  " + company + " is : " + totalEmpWage);
+
+        }
+        public static void Main()
+        {
+            computeEmpWage("NOKIA", 20, 2, 10);
+            computeEmpWage("SAMSUNG", 10, 4, 20);
+
         }
 
     }
