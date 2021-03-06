559e3958404c928ee39c8631fd659aa54b9a100a
akshayraut3105 <akshayraut3105@gmail.com>
2021-03-06 23:34:40 +0530

    [AKSHAY] added saving of wage for multiple compony

--- EmployeeWageNew/Program.cs
+++ EmployeeWageNew/Program.cs
@@ -1,47 +1,23 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployeeWageNew;
 
-namespace EmployeeWageNew
+namespace EmployeeWage
 {
-
-
-    class Progran
+    class Program
     {
-        //Constants
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 20;
-        public const int MAX_HRS_IN_MONTH = 100;
-        public static void Main()
+        static void Main(string[] args)
         {
-            //variables
-            int empHrs = 0;
-            int totalWorkingDays = 0;
-            int totalEmpHrs = 0;
-            while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day#" + totalWorkingDays + "EmpHrs:" + empHrs);
-            }
-            Console.WriteLine(totalEmpHrs + "   " + totalWorkingDays);
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage: " + totalEmpWage);
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("NOKIA", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("SAMSUNG", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.ToString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.ToString());
+            Console.ReadKey();
         }
 
     }
