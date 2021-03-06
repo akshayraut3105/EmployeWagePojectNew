365aa25ae7bd9a61169758735950c3d9451bd14f
Akshay Suresh Raut <79091948+akshayraut3105@users.noreply.github.com>
2021-03-06 23:48:53 +0530

    Update Program.cs

--- EmployeeWageNew/Program.cs
+++ EmployeeWageNew/Program.cs
@@ -6,55 +6,18 @@ using System.Threading.Tasks;
 
 namespace EmployeeWage
 {
-    class EmpWageBuilderObject
+    class Program
     {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-
-        private string company;
-        private int empRatePerHour;
-        private int numOfWorkingDays;
-        private int maxHoursPerMonth;
-        private int totalEmpWage;
-        public EmpWageBuilderObject(string company, int empRatePerHour,int numOfWorkingDays, int maxHoursPerMonth)
+        static void Main(string[] args)
         {
-            this.company = company;
-            this.empRatePerHour = empRatePerHour;
-            this.numOfWorkingDays = numOfWorkingDays;
-            this.maxHoursPerMonth = maxHoursPerMonth;
+            EmpWageBuilderObject dMart = new EmpWageBuilderObject("DMart", 20, 2, 10);
+            EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance", 10, 4, 20);
+            dMart.computeEmpWage();
+            Console.WriteLine(dMart.ToString());
+            reliance.computeEmpWage();
+            Console.WriteLine(reliance.ToString());
+            Console.ReadKey();
         }
-
-        public void computeEmpWage()
-        {
-            //variables
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-
-            //computation
-            while (totalEmpHrs <= this.maxHoursPerMonth && totalWorkingDays < this.numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Days#: " + totalWorkingDays + " Emp Hrs : " + empHrs);
-
-            }
-            totalEmpWage = totalEmpHrs * this.empRatePerHour;
-            Console.WriteLine("Total Emp Wage for Company : " + company + " is: " + totalEmpWage);
-        }
-
 
     }
 }
